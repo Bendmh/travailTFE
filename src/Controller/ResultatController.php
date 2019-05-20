301c844e2fd3b865e3e07d1e0b937b928075e0db
Bendmh <HE201458@students.ephec.be>
2019-05-20 23:25:35 +0200

    chaque professeur récupère les résultats de ses propres élèves

--- src/Controller/ResultatController.php
+++ src/Controller/ResultatController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\ResultSearch;
+use App\Entity\User;
 use App\Form\ResultSearchType;
 use App\Repository\ReponseEleveQCMRepository;
 use App\Repository\UserActivityRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,13 +18,19 @@ class ResultatController extends AbstractController
     /**
      * @Route("/resultats", name="resultat")
      */
-    public function index(UserActivityRepository $userActivityRepository, Request $request)
+    public function index(UserActivityRepository $userActivityRepository,Request $request)
     {
-
+        /** @var User $user */
+        $user = $this->getUser();
+        $classes = $user->getClasses();
+        $classesTableau = [];
+        foreach ($classes as $classe){
+            array_push($classesTableau, $classe->getNom());
+        }
         $search = new ResultSearch();
         $form = $this->createForm(ResultSearchType::class, $search);
         $form->handleRequest($request);
-        $user_activity = $userActivityRepository->findAllVisibleQuery($search);
+        $user_activity = $userActivityRepository->findAllVisibleQuery($search, $classesTableau);
         //$user_activity = $userActivityRepository->findAll();
         //$user_activity = $userActivityRepository->myFind(31);
 
